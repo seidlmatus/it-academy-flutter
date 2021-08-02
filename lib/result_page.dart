4bec7b53f34d4575ec2f739a81e7f50edc73a23d
Matus Seidl <seidl.matus@gmail.com>
2021-08-02 13:04:01 +0200

    result page

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled1/bottom_widget.dart';
 import 'package:untitled1/calculator.dart';
 import 'package:untitled1/input_page.dart';
+import 'package:untitled1/reusable_card.dart';
 
 class ResultPage extends StatelessWidget {
   static final String name = "/ResultPage";
@@ -12,13 +13,53 @@ class ResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String bmi = calculator.calculateBMI();
     return Scaffold(
       appBar: AppBar(
         title: Text('result'.toUpperCase()),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('${calculator.height}, ${calculator.weight}'),
+          Text(
+            'Your Result',
+            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              child: ReusableCard(
+                  contentChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        calculator.getResult(),
+                        style: TextStyle(
+                            color: Colors.greenAccent,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        bmi,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 100,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        calculator.getInfoText(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPress: () {}),
+            ),
+          ),
           BottomWidget(
             text: 'Re-calculate'.toUpperCase(),
             onTap: () {
